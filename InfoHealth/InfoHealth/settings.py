1161fcb3ca0ffd4076ca751495477dcdf1eda3bc
ShimaHAL <80742820+ShimaHAL@users.noreply.github.com>
2022-04-21 12:23:23 +0900

    envを分離

--- InfoHealth/InfoHealth/settings.py
+++ InfoHealth/InfoHealth/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_w!e)!9clq-vt0@&l#rizf+j1w6=p++tq)!w-+ecdqr(6)=v4s'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
